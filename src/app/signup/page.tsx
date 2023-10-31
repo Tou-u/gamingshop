f3bc9c94aeb6c203853f26e65dbf17388e68fc0e
Tou-u <rodrignacio99@hotmail.com>
2023-10-31 13:18:23 -0300

    init

---
+++ src/app/signup/page.tsx
@@ -0,0 +1,26 @@
+import Form from "@/components/form";
+import Link from "next/link";
+import { getPageSession } from "@/auth/lucia";
+import { redirect } from "next/navigation";
+
+const Page = async () => {
+  const session = await getPageSession();
+  if (session) redirect("/");
+  return (
+    <>
+      <h1>Sign up</h1>
+      <Form action="/api/signup">
+        <label htmlFor="username">Username</label>
+        <input name="username" id="username" />
+        <br />
+        <label htmlFor="password">Password</label>
+        <input type="password" name="password" id="password" />
+        <br />
+        <input type="submit" />
+      </Form>
+      <Link href="/login">Sign in</Link>
+    </>
+  );
+};
+
+export default Page;
