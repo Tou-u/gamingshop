774bc2386c63ad2e1cfe14d9f63e9ec45dd091af
Tou-u <rodrignacio99@hotmail.com>
2023-11-03 17:31:18 -0300

    Card para cada producto

--- src/components/ProductCard.tsx
+++ src/components/ProductCard.tsx
@@ -1,8 +1,9 @@
 "use client";
-import { Card, CardBody, CardFooter, Image } from "@nextui-org/react";
+import { Card, CardBody, CardFooter } from "@nextui-org/card";
+import { Image } from "@nextui-org/image";
 import NextLink from "next/link";
 
-type Products = {
+type Product = {
   id: string;
   slug: string;
   name: string;
@@ -11,40 +12,37 @@ type Products = {
   price: number;
   stock: number;
   category_id: string;
-}[];
+};
 
-export default function ProductCard({ products }: { products: Products }) {
+export default function ProductCard({ product }: { product: Product }) {
   return (
-    <div className="gap-2 grid grid-cols-3">
-      {products.map((product) => (
-        <Card
+    <Card
+      shadow="sm"
+      key={product.slug}
+      isPressable
+      as={NextLink}
+      href={`/product/${product.slug}`}
+      className="w-[200px]"
+    >
+      <CardBody className="overflow-visible p-0">
+        <Image
           shadow="sm"
-          key={product.slug}
-          isPressable
-          as={NextLink}
-          href={`/product/${product.slug}`}
-        >
-          <CardBody className="overflow-visible p-0">
-            <Image
-              shadow="sm"
-              radius="lg"
-              width="100%"
-              alt={product.name}
-              className="w-full object-cover h-[140px]"
-              src={product.image}
-            />
-          </CardBody>
-          <CardFooter className="text-small flex-col p-2 text-center">
-            <b>{product.name}</b>
-            <p className="text-default-500">
-              {Intl.NumberFormat("en-US", {
-                style: "currency",
-                currency: "USD",
-              }).format(product.price)}
-            </p>
-          </CardFooter>
-        </Card>
-      ))}
-    </div>
+          radius="lg"
+          width="100%"
+          alt={product.name}
+          className="w-full object-cover h-[140px]"
+          src={product.image}
+        />
+      </CardBody>
+      <CardFooter className="text-small flex-col p-2 text-center h-[100px] items-center justify-center">
+        <b>{product.name}</b>
+        <p className="text-default-500">
+          {Intl.NumberFormat("en-US", {
+            style: "currency",
+            currency: "USD",
+          }).format(product.price)}
+        </p>
+      </CardFooter>
+    </Card>
   );
 }
