79a36f1c65e0cdd5304ff9a192923e89340a32f7
Tou-u <rodrignacio99@hotmail.com>
2023-11-05 15:53:42 -0300

    corregido tipos

--- src/components/ProductCard.tsx
+++ src/components/ProductCard.tsx
@@ -1,18 +1,24 @@
-"use client";
-import { Card, CardBody, CardFooter } from "@nextui-org/card";
-import { Image } from "@nextui-org/image";
-import NextLink from "next/link";
+'use client'
+import { Card, CardBody, CardFooter } from '@nextui-org/card'
+import { Image } from '@nextui-org/image'
+import NextLink from 'next/link'
 
 type Product = {
-  id: string;
-  slug: string;
-  name: string;
-  description: string;
-  image: string;
-  price: number;
-  stock: number;
-  category_id: string;
-};
+  id: string
+  slug: string
+  name: string
+  description: string
+  image: string
+  price: number
+  stock: number
+  category_id: string
+  brand?: Brand
+}
+
+type Brand = {
+  id: string
+  name: string
+}
 
 export default function ProductCard({ product }: { product: Product }) {
   return (
@@ -22,8 +28,7 @@ export default function ProductCard({ product }: { product: Product }) {
       isPressable
       as={NextLink}
       href={`/product/${product.slug}`}
-      className="w-[200px]"
-    >
+      className="w-[200px]">
       <CardBody className="overflow-visible p-0">
         <Image
           shadow="sm"
@@ -31,18 +36,18 @@ export default function ProductCard({ product }: { product: Product }) {
           width="100%"
           alt={product.name}
           className="w-full object-cover h-[140px]"
-          src={product.image}
+          src="/placeholder.jpeg"
         />
       </CardBody>
       <CardFooter className="text-small flex-col p-2 text-center h-[100px] items-center justify-center">
         <b>{product.name}</b>
         <p className="text-default-500">
-          {Intl.NumberFormat("en-US", {
-            style: "currency",
-            currency: "USD",
+          {Intl.NumberFormat('en-US', {
+            style: 'currency',
+            currency: 'USD'
           }).format(product.price)}
         </p>
       </CardFooter>
     </Card>
-  );
+  )
 }
