8f41d5b49d4d7d9905a7b86bf815c3ee43d0a259
Tou-u <rodrignacio99@hotmail.com>
2023-12-01 17:53:11 -0300

    Información de la compra al abrir modal

--- src/app/(shop)/myorders/table.tsx
+++ src/app/(shop)/myorders/table.tsx
@@ -6,8 +6,11 @@ import { Button } from '@nextui-org/button'
 import { Link } from '@nextui-org/link'
 import NextLink from 'next/link'
 import { Prisma } from '@prisma/client'
-import { useState, Key } from 'react'
+import { useState, Key, Fragment } from 'react'
 import { Modal, ModalContent, ModalHeader, ModalBody, useDisclosure } from '@nextui-org/modal'
+import { CurrencyToUSD, LongDate, ShortDate } from '@/lib/utils'
+import { Address } from '@/types'
+import { Divider } from '@nextui-org/divider'
 
 type Order = {
   id: string
@@ -71,14 +74,7 @@ export default function TableComponent({ orders }: { orders: Order[] }) {
           </Chip>
         )
       case 'date':
-        return (
-          <p className="m-auto">
-            {new Intl.DateTimeFormat('es-MX', {
-              dateStyle: 'short',
-              timeZone: 'America/Santiago'
-            }).format(order.created_at)}
-          </p>
-        )
+        return <p className="m-auto">{ShortDate(order.created_at)}</p>
       case 'details':
         return (
           <Button
@@ -95,16 +91,66 @@ export default function TableComponent({ orders }: { orders: Order[] }) {
     }
   }
 
+  const renderAddress = (address: Address) => {
+    return (
+      <div className="grid grid-cols-5 w-full">
+        <h2 className="col-span-1">Buyer:</h2>
+        <p className="capitalize col-span-4">{`${address.first_name} ${address.last_name}`}</p>
+        <h2 className="col-span-1">Email:</h2>
+        <p className="col-span-4">{address.email}</p>
+        <h2 className="col-span-1">Phone:</h2>
+        <p className="col-span-4">9 {address.phone}</p>
+        <h2 className="col-span-1">Address:</h2>
+        <p className="col-span-4">{address.address}</p>
+        <h2 className="col-span-1">Info:</h2>
+        <p className="col-span-4">
+          {address.info === '' ? 'No additional information' : address.info}
+        </p>
+      </div>
+    )
+  }
+
+  const renderProducts = (products: Product[]) => {
+    const total = CurrencyToUSD(products.reduce((acc, p) => acc + p.price, 0))
+    return (
+      <div className="grid grid-cols-6 w-full">
+        {products.map((product) => (
+          <Fragment key={product.id}>
+            <p className="col-span-5">{product.name}</p>
+            <p>{CurrencyToUSD(product.price)}</p>
+          </Fragment>
+        ))}
+        <span className="col-span-4" />
+        <p>TOTAL:</p>
+        <p>{total}</p>
+      </div>
+    )
+  }
+
   return (
     <>
-      <Modal isOpen={isOpen} onOpenChange={onOpenChange} placement="center">
-        <ModalContent>
-          <ModalHeader className="flex flex-col gap-1">Order ID: {selectedData?.id}</ModalHeader>
-          <ModalBody>
-            <p>{JSON.stringify(selectedData?.address)}</p>
-          </ModalBody>
-        </ModalContent>
-      </Modal>
+      {selectedData && (
+        <Modal isOpen={isOpen} onOpenChange={onOpenChange} placement="center">
+          <ModalContent>
+            <ModalHeader className="pb-0 m-auto">Order ID: {selectedData.id}</ModalHeader>
+            <ModalBody>
+              <div className="px-2 flex flex-col gap-1">
+                <p>{`Purchase Date: ${LongDate(selectedData.created_at)}`}</p>
+                <Divider />
+                <section className="flex flex-col items-center">
+                  <h2 className="font-bold">Shipping Information</h2>
+                  {renderAddress(selectedData.address as Address)}
+                </section>
+                <Divider />
+                <section className="flex flex-col items-center">
+                  <h2 className="font-bold">Purchased Items</h2>
+                  {renderProducts(selectedData.products as Product[])}
+                </section>
+              </div>
+            </ModalBody>
+          </ModalContent>
+        </Modal>
+      )}
       <Table
         aria-label="Orders"
         removeWrapper
