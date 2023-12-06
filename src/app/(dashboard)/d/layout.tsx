7bbbe1f8504838adf0fe2fb53f35cc2007d14e86
Tou-u <rodrignacio99@hotmail.com>
2023-12-06 18:03:05 -0300

    Metadata default en dashboard

--- src/app/(dashboard)/d/layout.tsx
+++ src/app/(dashboard)/d/layout.tsx
@@ -1,10 +1,6 @@
 import { getPageSession } from '@/auth/lucia'
 import NavBar from './navbar'
 
-export const metadata = {
-  title: 'Gaming Shop Dashboard'
-}
-
 export default async function RootLayout({ children }: { children: React.ReactNode }) {
   const session = await getPageSession().catch((session) => (session = null))
   return (
