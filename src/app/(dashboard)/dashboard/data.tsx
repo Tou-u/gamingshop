e95ef7e7b759ffcac42dd6a8c71864e28eeade6e
Tou-u <rodrignacio99@hotmail.com>
2023-12-05 21:18:05 -0300

    Archivos removidos

--- src/app/(dashboard)/dashboard/data.tsx
+++
@@ -1,8 +0,0 @@
-import api from '@/lib/data/dashboard'
-import Table from './table'
-
-export default async function Data() {
-  const products = await api.getProductsDashboard()
-
-  return <>{products.data && <Table products={products.data} />}</>
-}
